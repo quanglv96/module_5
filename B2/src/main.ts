4528012d97a3bc1dfcc74fb53af706d5ee62879c
quanglv96 <quanglv1712@gmail.com>
2023-01-16 10:21:18 +0700

    b2

--- B2/src/main.ts
+++ B2/src/main.ts
@@ -1,6 +1,12 @@
-let width: number;
-let height:number;
-width=10.5;
-height=20;
-let area:number =width*height;
-console.log(`Diện tích hình chữ nhật : ${area}`);
+// let width: number;
+// let height:number;
+// width=10.5;
+// height=20;
+// let area:number =width*height;
+// console.log(`Diện tích hình chữ nhật : ${area}`);
+import {Rectangle} from "./rectangle";
+
+const rectangle = new Rectangle(5,5,10,20);
+console.log(rectangle.toString());
+console.log(rectangle.area());
+
