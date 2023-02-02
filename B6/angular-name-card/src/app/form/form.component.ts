5dd8bd71476931f864104478a137af0d3dcf51dc
quanglv96 <quanglv1712@gmail.com>
2023-02-02 08:23:45 +0700

    Fe angular crud 2 class

--- B6/angular-name-card/src/app/form/form.component.ts
+++ B6/angular-name-card/src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Form} from "../model/form";
 
 
@@ -9,13 +9,14 @@ import {Form} from "../model/form";
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  list: Form[] = [];
   newUser = new FormGroup({
-    user: new FormControl<any>('',[Validators.required]),
-    password: new FormControl<any>('',[Validators.required ,Validators.pattern("^[a-zA-Z0-9]{6,}")]),
+    user: new FormControl<any>('', Validators.required),
+    password: new FormControl<any>('', [Validators.required, Validators.pattern("^[a-zA-Z0-9]{6,}")]),
     email: new FormControl(),
     country: new FormControl(),
     gender: new FormControl(),
-    phone: new FormControl<any>('',[Validators.required,Validators.pattern("^[0-9]{10}")]),
+    phone: new FormControl<any>('', [Validators.required, Validators.pattern("^[0-9]{10}")]),
   })
   @Input() toString: String = "";
 
@@ -24,6 +25,7 @@ export class FormComponent {
 
   onSubmit(): void {
     let form = new Form(this.newUser.value.user, this.newUser.value.password, this.newUser.value.email, this.newUser.value.country, this.newUser.value.gender, this.newUser.value.phone);
-    this.toString = form.toString();
+    this.list.push(form);
+    // this.toString = form.toString();
   }
 }
